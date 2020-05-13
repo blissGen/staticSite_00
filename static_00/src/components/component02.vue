ee57308b1987d9945c1638cf7be8610559a9b90d
blissGenSTLT <bliss.gen.93@gmail.com>
2020-05-13 00:35:16 -0700

    default vue template

---
+++ static_00/src/components/component02.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="comp00">
+    <p :style="styleObject00">{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "component00",
+  data: function() {
+    return {
+      msg: "sweet",
+
+      styleObject00: {
+        textAlign: "center",
+        fontWeight: "thin"
+      }
+    };
+  }
+};
+</script>
